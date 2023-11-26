fbe0a4a0dad38acded0c2155120e9c56074a8ed8
sanjay <sanjayjustforyou123@gmail.com>
2023-11-26 14:48:37 +0530

    added html and database file

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-from flask import Flask, jsonify,request
+from flask import Flask, jsonify,request,render_template,redirect
 import ipl
-
+from database import Database
+dbo = Database()
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello World"
+@app.route("/")
+def index():
+    return render_template("login.html")
 
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/perform_registration", methods=['post'])
+def perform_registration():
+    email = request.form.get('user_ka_email')
+    name = request.form.get("user_ka_name")
+    password = request.form.get('user_ka_password')
+    response = dbo.insert(name,email,password)
+    if(response == 1):
+        return render_template("Login.html",message='Registration Successful. Kindly Login to procced')
+    else:
+        return render_template("register.html", message="Email already Exists")
+
+@ app.route("/perform_login", methods=['post'])
+def perform_login():
+    email = request.form.get('user_ka_email')
+    password = request.form.get('user_ka_password')
+    response = dbo.search(email,password)
+    if response == 1:
+        return redirect("/use_api")
+    else:
+        return render_template("login.html",message="incorrect email/password")
+
+@app.route("/use_api")
+def use_api():
+    return render_template("useAPI.html")
 @app.route('/api/teams')
 def teams():
     teams = ipl.teamsAPI()
